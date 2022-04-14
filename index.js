242136a014b885a190a72e2b9c6c49b053c28be2
Arturo Mares Sánchez <artumares08@gmail.com>
2022-04-14 14:51:15 -0500

    Versión de desarrollo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");   
 const { dbConnection } = require("./db/config.js");
+const res = require("express/lib/response");
 require("dotenv").config();
 
 
@@ -18,7 +20,10 @@ app.use(cors() );
 app.use(express.json());
 //Rutas
 app.use('/api/auth', require("./routes/auth.js"));
-
+// Manejar demás rutas
+app.get( "*", (req,res) =>{
+    res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
 
 app.listen(process.env.PORT, ()=>{
     console.log(`servidor corriendo en puerto ${4000}`)
